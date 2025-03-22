b6ca86ef23a3b193acd57ef2828ae5e8e14e2b53
Anubis.li <songling.li@brgroup.com>
2025-03-22 19:02:20 +0800

    更改配置 先移除数据库加载 保证正常启动

--- src/main/java/com/anubis/li/searchengine/SearchEngineApplication.java
+++ src/main/java/com/anubis/li/searchengine/SearchEngineApplication.java
@@ -2,8 +2,9 @@ package com.anubis.li.searchengine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SearchEngineApplication {
 
     public static void main(String[] args) {
